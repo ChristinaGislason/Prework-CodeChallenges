2e66dfc49c644e784a4310643a5323cc13edaab0
Christina Gislason <christinamkcho@gmail.com>
2018-10-07 16:06:56 -0700

    Problem 1 working

---
+++ Prework-CodeChallenges/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Prework_CodeChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem1();
+            //Problem2();
+            //Problem3();
+            //Problem4();
+        }
+
+        static void Problem1() 
+        {
+            //request user to input 5 numbers between 1-10
+            int[] userArray = new int[5];
+            int favNumber;
+
+            for (int i = 0; i < userArray.Length; i++)
+                {
+                    Console.WriteLine("Pick a number: ");
+                    favNumber = Convert.ToInt32(Console.ReadLine());
+
+                    userArray[i] = favNumber;
+                }
+            
+            //output array to the console
+            Console.WriteLine(string.Join(",", userArray));
+
+            //ask user to select a number
+            Console.Write("Select a number: ");
+            int selectedNum = Convert.ToInt32(Console.ReadLine());
+
+            //output score of chosen number as a product of that number multiplied by its occurence
+            int occurence = 0;
+            for (int i = 0; i < userArray.Length; i++)
+            {
+                if(userArray[i] == selectedNum)
+                {
+                    occurence++;
+                }
+            }
+            Console.WriteLine(selectedNum * occurence);
+        }
+
+
+        /*static void Problem2()
+        {
+            //get input of year
+            //determine if year is divisible by 4
+            //except every year that is evenly divisible by 100
+            //unless the year is also evenly divisible by 400
+        }
+        */
+
+        /*static void Problem3()
+        { 
+            //Initialize sequence
+            int[] sequence = { 1, 3, 2 };
+
+            //Return 'Yes' if array is perfect sequence; 
+            //Return 'No if array is not a perfect sequence
+            for (int i = 0; i < sequence.Length; i++)
+            {
+
+            }
+
+  
+        }
+        */
+          
+        //{
+        //    //PROBLEM 4
+        
+        //    //Get a matrix of integers
+
+        //    //Use for loops to loop through an array to get the sum of the array
+
+        //    //Return sum of each row in a single dimensional integer array
+        
+
+        //}
+
+}
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Prework-CodeChallenges
 Code Challenges for Prework
+
+## Problem 1: Array Max Result
+* Instructions of Challenge: Create a console application that requests 5 numbers between 1-10. 
+Output the array to the console and ask the user to select a number. 
+After the selection, output the "score" of the number chosen. 
+
+## Problem 2: Leap Year Calendar
+* Instructions of Challenge: Given a year, report if it is a leap year.
+
+## Problem 3: Perfect Squence
+* Instructions of Challenge:
+
+## Problem 4: Sum of Rows
+* Instructions of Challenge: Given a matrix of integers. Return the sum of each row in a single dimensional array.
